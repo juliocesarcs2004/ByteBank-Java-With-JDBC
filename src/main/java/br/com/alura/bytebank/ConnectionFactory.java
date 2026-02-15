50147d943dba3d5b7dcebcb7ee98293a8a39d446
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-02-14 23:34:35 -0300

    Created Conncetion Factory an use for INSERT data on database

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,21 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoDB {
+public class ConnectionFactory {
 
-    public static void main(String[] args) {
+    public Connection recuperaConexao() {
         try {
-          Connection connection = DriverManager
+            return DriverManager
                     .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=root");
-
-            System.out.println("Recuperei a conexão");
-
-            connection.close();
-
         } catch (SQLException e) {
-            System.out.println(e);
+            throw new RuntimeException(e);
         }
-
     }
-
 }
