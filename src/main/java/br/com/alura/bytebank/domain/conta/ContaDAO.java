70aee583ebcd8446c27a327b03741efb6066943c
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-02-15 10:49:33 -0300

    Added list method

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -1,11 +1,15 @@
 package br.com.alura.bytebank.domain.conta;
 
 import br.com.alura.bytebank.domain.cliente.Cliente;
+import br.com.alura.bytebank.domain.cliente.DadosCadastroCliente;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContaDAO {
 
@@ -36,6 +40,33 @@ public class ContaDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Set<Conta> listar() {
+        Set<Conta> contas = new HashSet<>();
+
+        String sql = "SELECT * FROM conta";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet resulteSet = ps.executeQuery();
+
+            while(resulteSet.next()){
+                Integer numero = resulteSet.getInt(1);
+                BigDecimal saldo = resulteSet.getBigDecimal(2);
+                String nome = resulteSet.getString(3);
+                String cpf = resulteSet.getString(4);
+                String email = resulteSet.getString(5);
+                DadosCadastroCliente dadosCadastroCliente =
+                        new DadosCadastroCliente(nome, cpf, email);
+                Cliente cliente = new Cliente(dadosCadastroCliente);
+                contas.add(new Conta(numero, cliente));
+            }
+
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return  contas;
     }
 
 }
