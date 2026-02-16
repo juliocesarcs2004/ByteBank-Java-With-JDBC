bef256cc36bc18ffa27c5bb17fbe12e97ae451ec
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2026-02-16 13:31:33 -0300

    Added Transactions

--- src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
+++ src/main/java/br/com/alura/bytebank/domain/conta/ContaDAO.java
@@ -114,17 +114,25 @@ public class ContaDAO {
         String sql = "UPDATE conta SET saldo = ? WHERE numero = ?";
 
         try {
+            conn.setAutoCommit(false);
+
             ps = conn.prepareStatement(sql);
 
             ps.setBigDecimal(1, valor);
             ps.setInt(2, numero);
 
             ps.execute();
+            conn.commit();
+
             ps.close();
             conn.close();
         } catch (SQLException e){
+            try {
+                conn.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(e);
+            }
             throw new RuntimeException(e);
         }
-
     }
 }
